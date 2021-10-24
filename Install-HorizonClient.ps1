e6c025ce7083a91f7c5ae103ba5185df8c979dd9
omnimod <34818199+omnimod@users.noreply.github.com>
2021-10-24 15:54:27 +0300

    Add files via upload

---
+++ Install-HorizonClient.ps1
@@ -0,0 +1,49 @@
+Param(
+		[Parameter(Position=0,
+		HelpMessage="Please enter the path to the Horizon Client installation file")]
+		[String] $FilePath = (Get-Location).ToString() + "\VMware-Horizon-Client-2106-8.3.0-18287501.exe",
+		
+		[Parameter(Position=1,
+		HelpMessage="additional arguments, check https://docs.vmware.com/en/VMware-Horizon-Client-for-Windows/2106/horizon-client-windows-installation/GUID-2DDF9C24-A1E9-4357-A832-2A5A19352D61.html for details")]	
+		[String] $ArgumentList = "ADDLOCAL=ALL /silent /norestart",
+
+		[Parameter(Position=2,
+		HelpMessage="Please specify if older Horizon Client should be updated")]		
+		[ValidateSet("CSV", "HTML", "TEXT")]
+		[switch]$UpdateOldClients = $true,
+	
+		[Parameter(Position=3,
+		HelpMessage="Please specify the maximum number of rows to output. If not defined, the default value is 1000")]
+		[String] $Logfile = "$env:windir\Temp\" + "horizon-client-autoinstall-script.txt"
+	)
+
+(Get-Date).DateTime.ToString() + " ============== Install-HorizonClient.ps1 ==============" | Out-File -Encoding utf8 $Logfile -Append
+
+#Check for installer file accessibility and version
+$FileVersion = (Get-Item $Filepath).VersionInfo.FileVersion
+
+if ($FileVersion -ne $null) {
+	(Get-Date).DateTime.ToString() + " Horizon Client installation file was found at $FilePath , Installer version: $FileVersion" | Out-File -Encoding utf8 $Logfile -Append
+} else {
+	(Get-Date).DateTime.ToString() + " Cannot access to Horizon Client installer at $FilePath Check for the installation file accessibility." | Out-File -Encoding utf8 $Logfile -Append
+	break
+}
+
+#Check if Horizon Client is installed
+$App = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall" | where {$_.GetValue("DisplayName") -eq "VMware Horizon Client"}
+
+if ($App -ne $null) {
+    $AppVersion = $App.GetValue("DisplayVersion")
+	(Get-Date).DateTime.ToString() + " Horizon Client $AppVersion is installed." | Out-File -Encoding utf8 $Logfile -Append	
+	
+    if (($UpdateOldClients -eq $true) -and ($AppVersion -lt $FileVersion)) {
+		(Get-Date).DateTime.ToString() + " Start Horizon Client installation." | Out-File -Encoding utf8 $Logfile -Append	
+        Start-Process -FilePath $Filepath -ArgumentList $ArgumentList
+    } else {
+		(Get-Date).DateTime.ToString() + " Old Horizon Client update is disabled or current version is installed. Check for installed version." | Out-File -Encoding utf8 $Logfile -Append	
+	}
+} else {
+	(Get-Date).DateTime.ToString() + " Horizon Client is not installed on this system. Start Horizon Client installation." | Out-File -Encoding utf8 $Logfile -Append
+    Start-Process -FilePath $Filepath -ArgumentList $ArgumentList	
+}
+
